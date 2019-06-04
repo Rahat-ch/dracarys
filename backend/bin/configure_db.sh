7c9a8f0b112beda45d6bf46d5a19c19e5c413ee2
Rahatch <40510125+Rahat-ch@users.noreply.github.com>
2019-06-04 13:54:58 -0400

    associated all tables but generation id not showing

--- backend/bin/configure_db.sh
+++ backend/bin/configure_db.sh
@@ -8,6 +8,7 @@ createdb -U node_user dracarysdb
 psql -U node_user dracarysdb <./bin/sql/generation.sql
 psql -U node_user dracarysdb <./bin/sql/dragon.sql
 psql -U node_user dracarysdb <./bin/sql/trait.sql
+psql -U node_user dracarysdb <./bin/sql/dragonTrait.sql
 
 node ./bin/insertTraits.js
 
